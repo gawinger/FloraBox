1a6097c45b3f8de1b2bffbe5dd225c3b2843639e
Bartosz Czupryna <gawinger1996@gmail.com>
2021-03-01 21:21:53 +0100

    Changed product image styling

--- public/scripts/product.js
+++ public/scripts/product.js
@@ -5,13 +5,18 @@ const imgZoom = document.querySelector(".product-img-zoom-container");
 const zoomedImg = document.querySelector(".product-img-zoom img");
 const closeZoomBtn = document.querySelector(".close-zoom-btn");
 const productThumbnails = document.querySelectorAll(".product-img-list img");
-const productImgList = document.querySelector(".product-img-list img");
+const productImgList = document.querySelector(".product-img-list");
 
 const setZoomedImgSrc = (e) => {
   zoomedImg.src = e.target.src;
   imgZoomContainer.classList.remove("hidden");
   window.addEventListener("click", closeZoom);
 };
+
+if (productThumbnails.length === 0) {
+  imgContainer.classList.add("img100");
+  productImgList.classList.add("hidden");
+}
 
 productImg.addEventListener("click", setZoomedImgSrc);
 productThumbnails.forEach((img) => {
@@ -21,7 +26,6 @@ productThumbnails.forEach((img) => {
 const closeZoom = (e) => {
   // if zoom is not hidden and
   // if user clicks on img hide navbar
-
   if (!imgZoom.contains(e.target) && e.target !== productImg && !document.querySelector(".product-images").contains(e.target)) {
     imgZoomContainer.classList.add("hidden");
     window.removeEventListener("click", closeZoom);
